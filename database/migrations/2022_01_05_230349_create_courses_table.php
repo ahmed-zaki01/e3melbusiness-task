6590d322591184dd91b0bd56d84f43568e7283fc
Ahmed Zaki <ahmed.zaki@aura-sys.com>
2022-01-06 01:16:30 +0200

    add required models and migrations.

---
+++ database/migrations/2022_01_05_230349_create_courses_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->nullable()->references('id')->on('categories')->onDelete('SET NULL');
+            $table->string('name', 60);
+            $table->text('description');
+            $table->integer('rating');
+            $table->integer('views');
+            $table->enum('levels', ['beginner', 'intermediate', 'high']);
+            $table->boolean('active')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+}
